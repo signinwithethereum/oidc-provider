ba45faf152a8e6d3caddf5fe82f94f8df1b4b7c8
jalil <jalil@1001.digital>
2026-03-18 22:49:33 +0100

    App config + env vars

--- app.config.ts
+++ app.config.ts
@@ -1,5 +1,9 @@
 export default defineAppConfig({
   evm: {
-    chains: ['mainnet', 'arbitrum', 'polygon'],
+    chains: {
+      mainnet: { id: 1, blockExplorer: 'https://etherscan.io' },
+      arbitrum: { id: 42161, blockExplorer: 'https://arbiscan.io' },
+      polygon: { id: 137, blockExplorer: 'https://polygonscan.com' },
+    },
   },
 })
