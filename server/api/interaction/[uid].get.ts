43d27c089dc76cc2b1e6bcd42b0ae2726302a9fb
jalil <jalil@1001.digital>
2026-03-19 10:52:49 +0100

    Extract client data (logo, uri, ...)

--- server/api/interaction/[uid].get.ts
+++ server/api/interaction/[uid].get.ts
@@ -6,6 +6,8 @@ export default defineEventHandler(async (event) => {
 
   try {
     const details = await provider.interactionDetails(req, res)
+    const client = await provider.Client.find(details.params.client_id as string)
+    const meta = client?.metadata()
     return {
       uid: details.uid,
       prompt: details.prompt,
@@ -13,6 +15,13 @@ export default defineEventHandler(async (event) => {
         client_id: details.params.client_id,
         scope: details.params.scope,
         redirect_uri: details.params.redirect_uri,
+      },
+      client: {
+        name: meta?.client_name,
+        logo_uri: meta?.logo_uri,
+        client_uri: meta?.client_uri,
+        policy_uri: meta?.policy_uri,
+        tos_uri: meta?.tos_uri,
       },
     }
   } catch (e) {
