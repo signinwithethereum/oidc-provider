63b1cb0cc0db1bccd0b88e460b5a830cdf7d8365
jalil <jalil@1001.digital>
2026-03-23 19:11:42 +0100

    Add server rendered preview

---
+++ app/components/SiweLogin.server.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="siwe-login">
+    <Loading
+      spinner
+      stacked
+      txt="Loading wallets"
+    />
+  </div>
+</template>
+
+<style scoped>
+.siwe-login {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: var(--spacer);
+  padding: var(--spacer);
+
+  > * {
+    inline-size: 100%;
+  }
+}
+</style>
